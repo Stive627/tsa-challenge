7db609736e9cb7cd25f7c34ec6ba751893c1b438
Stive627 <fossistive627@gmail.com>
2025-05-28 18:33:51 +0530

    last commit

--- Components/questions/Question.js
+++ Components/questions/Question.js
@@ -17,7 +17,7 @@ function Question({question, handleClick, lastQuestion}) {
       <div className=' w-11/12 lg:w-1/4'>
         <p style={{backgroundColor:'rgba(157, 78, 221, 1)', fontSize:16}} className=' text-white p-2 rounded-md mb-5'>{question?.question}</p>
         <div className=' flex flex-col gap-3 my-4'>
-          {question?.suggestions.map((elt, indx) => <button key={indx} onClick={()=>select(indx)} style={{borderColor:'rgba(157, 78, 221, 1)', color:indx === currSuggestion ? ' white':'rgba(157, 78, 221, 1)', backgroundColor:indx === currSuggestion?'rgba(157, 78, 221, 1)':'white'}} className=' py-1 border rounded-xl'><p>{alphabet[indx]} {elt}</p></button>)}
+          {question?.suggestions.map((elt, indx) => <button key={indx} onClick={()=>select(indx)} style={{borderColor:'rgba(157, 78, 221, 1)', color:indx === currSuggestion ? ' white':'rgba(157, 78, 221, 1)', backgroundColor:indx === currSuggestion?'rgba(157, 78, 221, 1)':'transparent'}} className=' py-1 border rounded-xl'><p>{alphabet[indx]} {elt}</p></button>)}
         </div>
         <button disabled={currSuggestion === undefined} onClick={() =>handleClick(currSuggestion)} className=' float-right cursor-pointer' style={{color:'rgba(181, 23, 158, 1)', fontSize:17}}>{lastQuestion?'Finish':'Next'}</button>
       </div>
--- Components/results/QuestionCard.js
+++ Components/results/QuestionCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import getColor from './getColor'
 
 function QuestionCard({question, answer, userAnswer}) {
  const alphabet = ['A)', 'B)', 'C)', 'D)', 'E)', 'F)']
@@ -7,7 +8,7 @@ function QuestionCard({question, answer, userAnswer}) {
         <div className=' w-11/12 lg:w-1/3'>
             <p style={{backgroundColor:'rgba(157, 78, 221, 1)', fontSize:16}} className=' text-white p-2 rounded-md mb-5'>{question.question}</p>
             <div className=' flex flex-col gap-3 my-4'>
-            {question.suggestions.map((elt, indx) => <button key={indx} style={{borderColor: indx === answer ? 'rgba(25, 175, 93, 1)':(((userAnswer !== answer) && indx === userAnswer)? 'rgba(235, 63, 66, 1)':'rgba(157, 78, 221, 1)'), color:indx === answer ? ' rgba(25, 175, 93, 1)':(((userAnswer !== answer) && indx === userAnswer)? 'rgba(235, 63, 66, 1)':'rgba(157, 78, 221, 1)')}} className=' py-1 border rounded-xl'><p>{alphabet[0]} {elt}</p></button>)}
+            {question.suggestions.map((elt, indx) => <button key={indx} style={{borderColor:getColor(indx, answer, userAnswer), color:getColor(indx, answer, userAnswer)}} className=' py-1 border rounded-xl'><p>{alphabet[0]} {elt}</p></button>)}
             </div>
         </div>
     </div>
